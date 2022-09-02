83c988ba2e188285252b9e0489605e08bab3ee56
AndrejGrabovac <101126091+AndrejGrabovac@users.noreply.github.com>
2022-09-02 19:23:47 +0200

    done

--- to-do/src/components/Form.js
+++ to-do/src/components/Form.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-const Form = ({ setInputText, setTodos, todos, inputText }) => {
+const Form = ({ setInputText, setTodos, todos, inputText, setStatus }) => {
   const inputTextHandler = (e) => {
-    console.log(e.target.value);
     setInputText(e.target.value);
   };
 
@@ -14,6 +13,11 @@ const Form = ({ setInputText, setTodos, todos, inputText }) => {
     ]);
     setInputText("");
   };
+
+  const statusHandler = (e) =>{
+    setStatus(e.target.value);
+  }
+
   return (
     <form>
       <input
@@ -26,7 +30,7 @@ const Form = ({ setInputText, setTodos, todos, inputText }) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
